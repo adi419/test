3f8bd70c01226180cf350c4f7f6487e6fac8863d
Adi <adi@Macintosh.local>
2016-03-07 15:06:36 -0500

    added hover function

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <Html>
 	<head>
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8">
@@ -19,7 +20,7 @@
 <body>
 	<p class="hello">Hello World</p>
 	<p class="hello">Hello World 2</p>
-	<p id="hello3">Hello World 3</p>
+	<p id="hello3">Hello World 3 </p>
 </body>
 <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
 <script type="text/javascript">
@@ -27,7 +28,9 @@
 $('#hello3').click(function(){
   $('.hello:first').toggle();
   $('.hello:last').toggle();
-  
+  $("#p1").hover(function(){
+          alert("You entered p1!");
+      },
   
 });
 </script>
