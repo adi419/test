0d18265656a2eb590aba4727b2eb77937519738e
adi419 <adimahendru419@gmail.com>
2016-03-12 16:00:43 -0500

    Create page4.php

---
+++ page4.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+
+<Html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+		<title>adis Sports</title>
+		<style type="text/css" media="screen">
+		a,abbr,acronym,address,applet,article,aside,audio,b,big,blockquote,body,canvas,caption,center,cite,code,dd,del,details,dfn,div,dl,dt,em,embed,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,header,hgroup,html,i,iframe,img,ins,kbd,label,legend,li,mark,menu,nav,object,ol,output,p,pre,q,ruby,s,samp,section,small,span,strike,strong,sub,summary,sup,table,tbody,td,tfoot,th,thead,time,tr,tt,u,ul,var,video{margin:0;padding:0;border:0;font:inherit;vertical-align:baseline}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:after,blockquote:before,q:after,q:before{content:'';content:none}table{border-collapse:collapse;border-spacing:0}
+		body{
+			color: yellow;
+			font-family: sans-serif,arial;
+			font-weight: bold;
+		}
+		.navigation{
+			list-style:none;
+			text-align: center;
+		}
+		
+		.navigation li{
+			display: inline-block;
+			margin: 20px;
+			padding: 20px;
+			text-transform: uppercase;
+		}
+		nav{
+			background-color: grey;
+			width:100%;
+			top: 0;
+			position: fixed;
+		}
+		header{
+			margin-top: 96px;
+		}
+		footer ul{
+			width: 33%;
+			display: inline-block;
+			height: 300px;
+			text-align:center;
+		}
+		footer{
+			background-color:grey;
+		}
+		.footer-header{
+			padding: 20px;
+			margin-top: 20px;
+		}
+		p{
+			color:black;
+			line-height: 2em;
+		}
+		section{
+			width:65%;
+			text-align: center;
+			margin: 20px auto;
+			
+		}
+		#section-header {
+		    background: none repeat scroll 0 0 orange;
+		      border-top: 20px solid;
+		      height: 50px;
+		      padding-top: 20px;
+		      width: 100%;
+			  margin-bottom: 20px;
+			  margin-top: -40px;
+		}
+		
+		.parallax-window {
+		    min-height: 692px;
+			width: 100%;
+			background-size: contain;
+		}
+		</style>
+					
+	</head>
+<body>
+	<nav>
+		<ul class='navigation'>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="page2.php">page2</a></li>
+			<li><a href="page3.php">page3</a></li>
+			<li><a href="page4.php">page4</a></li>
+			<li><a>Page 5</a></li>
+			<li><a>Page 6</a></li>
+		</ul>	
+	</nav>	
+	<header class="parallax-window" data-parallax="scroll" data-image-src="https://placehold.it/1385x692">
+	</header>
+	
+	<section>
+		<div id='section-header'>
+		</div>
+		<H1>Welcome to Page 3!</H1>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+		</p>
+		<p>
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+	</p>
+	<p>
+	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+	</p>
+	<p>
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+	</p>
+	</section>
+	<div class="parallax-window" data-parallax="scroll" data-image-src="https://placehold.it/1385x692"></div>
+	<footer> 
+		<ul><li class='footer-header'> Get in Touch </li> </ul>
+		<ul><li class='footer-header'> Latest News </li></ul>
+		<ul><li class='footer-header'> Get Connected </li> </ul>
+	</footer>
+	<script src='https://code.jquery.com/jquery-2.2.1.min.js'></script>
+	<script src="https://pixelcog.github.io/parallax.js/js/parallax.min.js"></script>
+</body>
+</html>
